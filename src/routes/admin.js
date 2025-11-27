5d074b60157268ef9ab1e8f605c081bbdab57687
TRISHA2000 <sayantani.k@vyomainnovusglobal.com>
2025-11-27 13:56:18 +0530

    done

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,6 +4,6 @@ import { getDepartureCount } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.post("/get-arrival-departure-count", getDepartureCount);
+router.post("/arrival-departure-count", getDepartureCount);
 
 export default router;
